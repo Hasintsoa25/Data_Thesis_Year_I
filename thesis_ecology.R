b712794d84bbd62079460907bc10c2060fbf305c
Hasintsoa <hasinitsoa97@gmail.com>
2024-11-27 09:57:35 +0300

    data analysis first step

--- thesis_ecology.R
+++ thesis_ecology.R
@@ -1,1 +1,13 @@
-library(tidyverse)
+# importation de spackage por la manipulation des données-----
+ library(tidyverse)
+
+# DAATAAAAAAATTTTTTTTTTTTTTTTTAAAAAAAAAAAAAAAAA
+data = read.csv("data_Thesis_I.csv")
+data |> str()#structure
+data |> skimr::skim()
+data |> dlookr::normality()
+
+## COMMMMMEEENNNNTTTTT PROCEDONS NOUS ALORS??????
+### Abondance et divrsite globales (ecosystem)
+### Abondance et indices pour chaque unités
+
