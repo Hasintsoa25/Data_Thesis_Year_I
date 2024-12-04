77c275f0048bfc49871d046075200e43d74fc214
Hasintsoa <123290827+Hasintsoa25@users.noreply.github.com>
2024-12-04 15:11:14 +0300

    Add files via upload
    Ecosystem service

---
+++ ES_script.R
@@ -0,0 +1,12 @@
+################ Ecosysteme Services###################
+library(tidyverse)
+es<- read.csv("ES_survey.csv")
+
+
+# Ordre par valeur d'importance des S.E (reperteriorer les especes)
+#Mis en pourcentage des index ethnobotany 
+#Mis en forme des données 
+## 1 -  Je crois que  on doit categoriser les ES par régulation, approvisonnement,
+#soutient et cilturel pour chaque type d'écosystème
+#
+View(es)
